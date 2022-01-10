c40524b225274846b3e46173e24119d1c7766aa1
tsukresha <92365911+tsukresha@users.noreply.github.com>
2022-01-10 18:11:53 +0200

    Add files via upload

--- gulp.config.js
+++ gulp.config.js
@@ -5,6 +5,10 @@ module.exports = {
     'node_modules/normalize.css/normalize.css'
   ],
   JS_LIBS : [
-    'node_modules/jquery/dist/jquery.js'
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/jquery-touchswipe/jquery.touchSwipe.js',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js',
+    'node_modules/bxslider/dist/jquery.bxslider.js',
+    'node_modules/mobile-detect/mobile-detect.js'
   ]
  }
