dd5e17d65ea8385fbc588ca240232ed12706bf24
tsukresha <92365911+tsukresha@users.noreply.github.com>
2022-01-10 18:52:52 +0200

    Add files via upload

--- gulp.config.js
+++ gulp.config.js
@@ -2,7 +2,7 @@ module.exports = {
   SRC_PATH : 'src',
   DIST_PATH : 'dist',
   STYLE_LIBS : [
-    'node_modules/normalize.css/normalize.css'
+    // 'node_modules/normalize.css/normalize.css'
   ],
   JS_LIBS : [
     'node_modules/jquery/dist/jquery.js',
